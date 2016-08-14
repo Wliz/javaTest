8f6b8a5d190b7a8aea1aa1a202e79bd799c86988
jack_fan <a985850124@qq.com>
2016-08-14 17:07:57 +0800

    add bubble Sort

---
+++ src/main/java/排序/冒泡排序/BubbleSort.java
@@ -0,0 +1,43 @@
+package 排序.冒泡排序;
+
+/**
+ * 冒泡排序是稳定的
+ * @author a9858
+ *
+ */
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] a={49,38,65,97,76,13,27,49,78,34,12,64,1,8};
+        System.out.println("排序之前：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        
+        System.out.println();
+        long startTime = System.currentTimeMillis();
+        
+        //冒泡排序算法
+        for(int i = 0; i < a.length; i++){
+        	for(int j = 0; j < a.length - 1 - i; j++){
+        		//这里-i主要是每遍历一次都把最大的i个数沉到最底下去了，没有必要再替换了
+        		if(a[j] > a[j + 1]){
+        			int temp = a[j];
+        			a[j] = a[j + 1];
+        			a[j + 1] = temp;
+        		}
+        	}
+        }
+        
+        System.out.println();
+        
+        long endTime = System.currentTimeMillis();
+        System.out.println("the time of work is : " + (endTime - startTime));
+        
+        System.out.println("排序之后：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+
+
+	}
+}
