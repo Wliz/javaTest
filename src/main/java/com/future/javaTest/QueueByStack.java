777255d668e9d84118b24e72c4de55e182ece753
jack_fan <a985850124@qq.com>
2016-08-20 17:20:15 +0800

    build a queue with stack ,and write queue's pop method and push method

---
+++ src/main/java/com/future/javaTest/QueueByStack.java
@@ -0,0 +1,61 @@
+package com.future.javaTest;
+
+import java.util.Stack;
+
+/** 
+ * @ClassName: QueueByStack 
+ * @Description: 用栈来模拟队列，进行push和pop
+ * @author: a9858
+ * @date: 2016年8月20日 下午5:18:26  
+ */
+public class QueueByStack {
+
+	Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+	public static void main(String[] args){
+		QueueByStack queue = new QueueByStack();
+		queue.push(1);
+		queue.push(2);
+		
+		while(!queue.isEmpyt()){
+			System.out.println(queue.pop());
+		}
+	}
+	
+	/** 
+	 * @Title: isEmpyt 
+	 * @Description: 判断队列是否是空，用储存栈stack1来判断
+	 * @return: boolean
+	 */
+	public boolean isEmpyt(){
+		return stack1.isEmpty();
+	}
+	
+    /** 
+     * @Title: push 
+     * @Description: 压入队列 
+     * @param node
+     */
+    public void push(int node) {
+        stack1.push(node);
+    }
+    
+    /** 
+     * @Title: pop 
+     * @Description: 出队列
+     * @return: int
+     */
+    public int pop() {
+    	while(!stack1.isEmpty()){
+    		stack2.push(stack1.pop());
+    	}
+    	
+    	int result = stack2.pop();
+    	
+    	while(!stack2.isEmpty()){
+    		stack1.push(stack2.pop());
+    	}
+    	return result;
+    }
+}
