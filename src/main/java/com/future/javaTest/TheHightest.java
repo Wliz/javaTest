7d17aff6b36030a5483f0311e3463b4fc535f155
jack_fan <a985850124@qq.com>
2016-08-22 11:06:46 +0800

    a test of huawei

--- src/main/java/com/future/javaTest/TheHightest.java
+++ src/main/java/com/future/javaTest/TheHightest.java
@@ -2,6 +2,13 @@ package com.future.javaTest;
 
 import java.util.Scanner;
 
+/** 
+ * @ClassName: TheHightest 
+ * @Description: 华为笔试题，最高分是多少
+ * 不能处理多次输入的问题
+ * @author: a9858
+ * @date: 2016年8月22日 上午10:54:02  
+ */
 public class TheHightest {
 
 	public static void main(String[] args) {
@@ -9,11 +16,13 @@ public class TheHightest {
 		
 		while(scanner.nextLine() != null){
 			
+			//对第一行数进行处理，得到总人数n，总操作数m
 			String one = scanner.nextLine();
 			int n = Integer.parseInt((one.split(" ")[0]));
 			int m = Integer.parseInt((one.split(" ")[1]));
 			int[] courses = new int[n];
 			
+			//对第二行数进行处理，得到每个人的分数
 			String two = scanner.nextLine();
 			String[] twoStr = two.split(" "); 
 			for(int i = 0; i < n; i++){
@@ -21,7 +30,7 @@ public class TheHightest {
 			}
 			
 			int[] results = new int[m];
-			for(int i = 0; i < m; i++){
+			for(int i = 0; i < m; i++){//依次进行操作
 				String temp = scanner.nextLine();
 				String[] tempStr = temp.split(" ");
 				results[i] = doQuestion(courses,tempStr[0], tempStr[1],tempStr[2]);
@@ -35,6 +44,15 @@ public class TheHightest {
 		}
 	}
 
+	/** 
+	 * @Title: doQuestion 
+	 * @Description: 进行操作，如果第一个字符是Q，则返回最大数，否则对分数进行处理
+	 * @param courses 分数数组，
+	 * @param string 操作的第一个字符
+	 * @param string2 操作的第二个字符
+	 * @param string3 操作的第三个字符
+	 * @return: int 最高的分数
+	 */
 	private static int doQuestion(int[] courses, String string, String string2, String string3) {
 		int a = Integer.parseInt(string2);
 		int b = Integer.parseInt(string3);
