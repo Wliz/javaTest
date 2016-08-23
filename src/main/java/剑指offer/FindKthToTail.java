ccfb0b838d61c7c064bd4380f124a80dd8881503
jack_fan <a985850124@qq.com>
2016-08-23 20:50:26 +0800

    add another method ，a great method

--- src/main/java/剑指offer/FindKthToTail.java
+++ src/main/java/剑指offer/FindKthToTail.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 /** 
  * @ClassName: FindKthToTail 
- * @Description: 输入一个链表，输出该链表中倒数第k个结点，注意细节
+ * @Description: 输入一个链表，输出该链表中倒数第k个结点，注意细节，第二种方法甚妙
  * @author: a9858
  * @date: 2016年8月23日 下午5:18:31  
  */
@@ -22,7 +22,8 @@ public class FindKthToTail {
 //			head = head.next;
 //		}
 		
-		System.out.println(FindKthToTail(head,1).val);
+//		System.out.println(FindKthToTail(head,1).val);
+		System.out.println(findtheNode(head,2).val);
 	}
 	
 	/** 
@@ -59,5 +60,40 @@ public class FindKthToTail {
 		
 		return temp;
     }
+	
+	/** 
+	 * @Title: findtheNode 
+	 * @Description: 两个变量来控制链表的查找，pre先向前移动k位，<br>
+	 * 			   	 然后pre和last两个同时向后移动，直到pre到达末尾，<br>
+	 *   			 这时候last就处在相对于末尾前面k的位置上<br>
+	 * @param head
+	 * @param k
+	 * @return: ListNode
+	 */
+	public static ListNode findtheNode(ListNode head, int k){
+		
+		if(head == null || k <= 0){//处理不常规的数据
+			return null;
+		}
+		
+		ListNode pre = head;
+		ListNode last = head;
+		
+		for(int i = 1; i < k;i++){//先向前到达第k个位置
+			if(pre.next != null){
+				pre = pre.next;
+			}else{//说明k超过链表的长度
+				return null;
+			}
+		}
+		
+		while(pre.next != null){//再同时向前，找到相对于末尾前第k个位置
+			pre = pre.next;
+			last = last.next;
+		}
+		
+		
+		return last;
+	}
 
 }
